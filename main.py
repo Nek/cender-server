e9fb6a94f3fdc88fc71694db729c0e3f4185714e
Nikita Anatolievich Dudnik <nikdudnik@gmail.com>
2024-09-01 12:10:46 +0200

    Make it work

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from app.core import greet
+import zerorpc
 
-def main():
-    name = input("Enter your name: ")
-    print(greet(name))
+from transit.writer import Writer
+from transit.reader import Reader
+from io import StringIO
 
-if __name__ == "__main__":
-    main()
+
+
+class MyService:    
+    def echo(self, args):
+        reader = Reader("json") # or "msgpack"
+        incoming_args = reader.read(StringIO(args)) # Decode args
+        print(incoming_args)
+        return_value = StringIO()
+        writer = Writer(return_value, "json") # or "json-verbose", "msgpack"
+        writer.write(incoming_args) # Encode decoded args into the return_value
+        return return_value.getvalue()
+
+server = zerorpc.Server(MyService())
+server.bind("tcp://127.0.0.1:10000")
+server.run()
