36cce12ee96d0d0d1940d7be3dcbd672e74a358e
Nikita Dudnik (nek) <nikdudnik@gmail.com>
2024-09-15 12:54:56 +0200

    Dump experiment

--- rpc_server_addon/blender_datatypes.py
+++ rpc_server_addon/blender_datatypes.py
@@ -17,9 +17,29 @@ class VectorWriter(object):
     @staticmethod
     def string_rep(a):
         return None
+    
+class ColorReader(object):
+    @staticmethod
+    def from_rep(v):
+        return mathutils.Color(v)
+
+class ColorWriter(object):
+    @staticmethod
+    def tag(a):
+        return 'blender-color'
+
+    @staticmethod
+    def rep(a):
+        return a[:]
+
+    @staticmethod
+    def string_rep(a):
+        return None
 
 def register_readers(reader):
     reader.register("blender-vector", VectorReader)
+    reader.register("blender-color", ColorReader)
 
 def register_writers(writer):
     writer.register(mathutils.Vector, VectorWriter)
+    writer.register(mathutils.Color, ColorWriter)
--- rpc_server_addon/blender_workspace.py
+++ rpc_server_addon/blender_workspace.py
@@ -9,13 +9,6 @@ def move_object(object_name, pos):
     obj.location = pos                                                                                                          
     return f"Moved '{object_name}' to position ({pos.x}, {pos.y}, {pos.z})"
 
-def set_obj_prop(object_name, prop_name, value):
-    obj = bpy.data.objects.get(object_name)                                                                                                              
-    if obj is None:                                                                                                                                      
-        return f"Object '{object_name}' not found"
-    setattr(obj, prop_name, value)
-    return f"Set '{prop_name} of '{object_name}' to '{value}'"
-
 def eval_code(code):
     return eval(code)
 
@@ -27,3 +20,35 @@ def set_var(name, val):
 
 def get_var(name):
     return getattr(sys.modules[__name__], name)
+
+# objects, materials
+def set_item_prop(collection, item_name, prop_name, value):
+    item = getattr(bpy.data, collection).get(item_name)                                                                                                              
+    if item is None:                                                                                                                                      
+        return f"Item '{item_name}' of '{collection}' not found"
+    setattr(item, prop_name, value)
+    return f"Set '{prop_name} of '{item_name}' of '{collection}' to '{value}'"
+
+def get_item_prop(collection, item_name, prop_name):
+    item = getattr(bpy.data, collection).get(item_name)                                                                                                            
+    if item is None:                                                                                                                                      
+        return f"Item '{item_name}' of '{collection}' not found"
+    return getattr(item, prop_name)
+
+def set_obj_prop(object_name, prop_name, value):
+    obj = bpy.data.objects.get(object_name)                                                                                                              
+    if obj is None:                                                                                                                                      
+        return f"Object '{object_name}' not found"
+    setattr(obj, prop_name, value)
+    return f"Set '{prop_name} of '{object_name}' to '{value}'"
+
+def get_obj_prop(object_name, prop_name):
+    obj = bpy.data.objects.get(object_name)                                                                                                              
+    if obj is None:                                                                                                                                      
+        return f"Object '{object_name}' not found"
+    return getattr(obj, prop_name)
+
+def add_cube(name, size=1):
+    bpy.ops.mesh.primitive_cube_add(size=size)
+    bpy.context.object.name = name
+    return bpy.context.object.name
