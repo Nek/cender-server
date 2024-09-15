c1c0c88652224156aa21824d315d6d998c77f97b
Nikita Dudnik (nek) <nikdudnik@gmail.com>
2024-09-15 05:18:14 +0200

    fix: Register Vector type with transit reader

--- rpc_server.py
+++ rpc_server.py
@@ -13,27 +13,29 @@ from bpy.utils import register_class, unregister_class
 
 from transit.writer import Writer
 from transit.reader import Reader
+from transit.decoder import Tag
+
 from io import StringIO
 
 import sys
 
-def move_object(object_name: str, x: float, y: float, z: float):
+def move_object(object_name: str, pos):
     obj = bpy.data.objects.get(object_name)
     if obj is None:
         return f"Object '{object_name}' not found"
     
-    obj.location = mathutils.Vector((x, y, z))
-    return f"Moved '{object_name}' to position ({x}, {y}, {z})"
-
+    obj.location = pos
+    return f"Moved '{object_name}' to position ({pos})"
 
 class RPCService:
     def __init__(self, address, port):
         self.address = address
         self.port = port
+        self.reader = Reader("json")  # or "msgpack"
+        self.reader.register("Vector", lambda vals: mathutils.Vector(vals))
 
     def call_fn(self, fn_args):
-        reader = Reader("json")  # or "msgpack"
-        incoming_fn_args = reader.read(StringIO(fn_args))  # Decode args
+        incoming_fn_args = self.reader.read(StringIO(fn_args))  # Decode args
         fn, *args = incoming_fn_args
         print(f"Calling function: {fn} with args: {args}")
         res = getattr(sys.modules[__name__], fn)(*args)         
@@ -41,7 +43,9 @@ class RPCService:
         writer = Writer(return_value, "json")  # or "json-verbose", "msgpack"
         writer.write(res)  # Encode decoded args into the return_value
         return return_value.getvalue()
-
+        
+    def eval_code(self, code: str):
+        return eval(code)
 
 server = None
 server_thread = None
